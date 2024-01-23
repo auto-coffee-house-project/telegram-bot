78befebd0ad02ff9c4a9b0aea33b0b14f6b0e472
Eldos <eldos.baktybekov@gmail.com>
2024-01-23 17:59:55 +0600

    :label: Add alias

--- src/models/salesmans.py
+++ src/models/salesmans.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 __all__ = ('Salesman',)
 
 
 class Salesman(BaseModel):
     id: int
-    user_id: int
+    user_id: int = Field(alias='user')
