bc9a80c3ff8ead44befcb16de58b10e157ddbbc0
Eldos <eldos.baktybekov@gmail.com>
2024-01-23 23:58:45 +0600

    :technologist: Add types for bot repository, handle errors

--- src/repositories/bots.py
+++ src/repositories/bots.py
@@ -1,3 +1,8 @@
+from pydantic import TypeAdapter
+
+from exceptions import ServerAPIError
+from models import Bot
+from parsers import parse_api_response
 from repositories.base import APIRepository
 
 __all__ = ('BotRepository',)
@@ -5,8 +10,14 @@ __all__ = ('BotRepository',)
 
 class BotRepository(APIRepository):
 
-    async def get_all(self) -> list[dict]:
+    async def get_all(self) -> list[Bot]:
         url = '/telegram/bots/'
         response = await self._http_client.get(url)
-        # TODO check response
-        return response.json()
+
+        api_response = parse_api_response(response)
+
+        if api_response.ok:
+            type_adapter = TypeAdapter(list[Bot])
+            return type_adapter.validate_python(api_response.result)
+
+        raise ServerAPIError(response)
