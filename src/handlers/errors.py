438c5bb7e8eae492ff544ad1e8d7b066bdbf9d4a
Eldos <eldos.baktybekov@gmail.com>
2024-01-24 16:17:41 +0600

    :construction: WIP error handlers

--- src/handlers/errors.py
+++ src/handlers/errors.py
@@ -8,10 +8,12 @@ router = Router(name=__name__)
 
 
 @router.error(ExceptionTypeFilter(ServerAPIError))
-async def on_server_api_error(event: ErrorEvent, **kwargs) -> None:
-    print(kwargs)
+async def on_server_api_error(event: ErrorEvent) -> None:
+    if event.update.message is not None:
+        await event.update.message.answer('Произошла ошибка на сервере')
 
 
 @router.error(ExceptionTypeFilter(ResponseJSONParseError))
 async def on_response_json_parse_error(event: ErrorEvent, **kwargs) -> None:
-    print(kwargs)
+    if event.update.message is not None:
+        await event.update.message.answer('Произошла ошибка при обработке ответа от сервера')
