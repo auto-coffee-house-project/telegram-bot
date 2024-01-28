4feec4c9686ff02944b7cc4469904c88c35b0dd4
Eldos <eldos.baktybekov@gmail.com>
2024-01-28 10:39:02 +0600

    :children_crossing: Add reset state in start handlers

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,5 +1,6 @@
 from aiogram import Router, Bot
 from aiogram.filters import CommandStart, StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from filters import (
@@ -11,7 +12,8 @@ from views import (
     StartClientView,
     answer_photo_view,
     StartSalesmanView,
-    answer_view, StartAdminView,
+    answer_view,
+    StartAdminView,
 )
 
 __all__ = ('router',)
@@ -26,9 +28,11 @@ router = Router(name=__name__)
 )
 async def on_start_salesman(
         message: Message,
+        state: FSMContext,
 ) -> None:
     view = StartSalesmanView()
     await answer_view(message, view)
+    await state.clear()
 
 
 @router.message(
@@ -38,9 +42,11 @@ async def on_start_salesman(
 )
 async def on_start_admin(
         message: Message,
+        state: FSMContext,
 ) -> None:
     view = StartAdminView()
     await answer_view(message, view)
+    await state.clear()
 
 
 @router.message(
@@ -50,9 +56,11 @@ async def on_start_admin(
 )
 async def on_start_client(
         message: Message,
+        state: FSMContext,
         bot: Bot,
         shop_group_repository: ShopGroupRepository,
 ) -> None:
     shop_group = await shop_group_repository.get_by_bot_id(bot.id)
     view = StartClientView(shop_group.each_nth_cup_free)
     await answer_photo_view(message, view)
+    await state.clear()
