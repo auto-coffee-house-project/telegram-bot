58bfa2d3c4f8acf3ce2d880d33263b6f537e8f9a
Eldos <eldos.baktybekov@gmail.com>
2024-01-28 10:38:15 +0600

    :label: Add salesman create response model

--- src/models/salesmans.py
+++ src/models/salesmans.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 
-__all__ = ('Salesman',)
+__all__ = ('Salesman', 'SalesmanCreateResponse')
 
 
 class Salesman(BaseModel):
     id: int
     user_id: int
+
+
+class SalesmanCreateResponse(BaseModel):
+    shop_name: str
+    shop_group_name: str
