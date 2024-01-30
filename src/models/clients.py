1bb01a81af1c09bfcb79abb39193c99cc5c36c80
Eldos <eldos.baktybekov@gmail.com>
2024-01-30 22:03:13 +0600

    :label: Update client purchases statistics

--- src/models/clients.py
+++ src/models/clients.py
@@ -3,7 +3,20 @@ from pydantic import BaseModel
 __all__ = ('ClientPurchasesStatistics',)
 
 
+class UserPartial(BaseModel):
+    id: int
+    first_name: str
+    last_name: str | None
+    username: str | None
+
+    @property
+    def full_name(self) -> str:
+        if self.last_name is None:
+            return self.first_name
+        return f'{self.first_name} {self.last_name}'
+
+
 class ClientPurchasesStatistics(BaseModel):
-    user_id: int
+    user: UserPartial
     total_purchases_count: int
     free_purchases_count: int
