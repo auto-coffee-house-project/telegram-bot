45fc2065d716712ab5f0f947d8377d3bf8f4739c
Eldos <eldos.baktybekov@gmail.com>
2024-01-30 21:13:10 +0600

    :alien: Add API call in bot repository

--- src/repositories/bots.py
+++ src/repositories/bots.py
@@ -1,6 +1,6 @@
 from pydantic import TypeAdapter
 
-from exceptions import ServerAPIError
+from exceptions import ServerAPIError, BotDoesNotExistError
 from models import Bot
 from parsers import parse_api_response
 from repositories.base import APIRepository
@@ -19,5 +19,19 @@ class BotRepository(APIRepository):
         if api_response.ok:
             type_adapter = TypeAdapter(list[Bot])
             return type_adapter.validate_python(api_response.result)
+
+        raise ServerAPIError(response)
+
+    async def get_by_id(self, bot_id: int) -> Bot:
+        url = f'/telegram/bots/{bot_id}/'
+        response = await self._http_client.get(url)
+
+        api_response = parse_api_response(response)
+
+        if api_response.ok:
+            return Bot.model_validate(api_response.result)
+
+        if api_response.message == 'Does not exist':
+            raise BotDoesNotExistError(bot_id)
 
         raise ServerAPIError(response)
