e4eea9fb2118f7df62ef01c08427e1ca9ac7cdb8
Eldos <eldos.baktybekov@gmail.com>
2024-02-01 22:03:30 +0600

    :sparkles: Add exception class

--- src/exceptions/admins.py
+++ src/exceptions/admins.py
@@ -2,3 +2,7 @@ class AdminDoesNotExistError(Exception):
 
     def __init__(self, admin_user_id: int):
         self.admin_user_id = admin_user_id
+
+
+class UserIsAlreadyAdminError(Exception):
+    pass
